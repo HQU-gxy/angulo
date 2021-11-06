5a82988c18e363eb7277774367751383f304478d
crosstyan <cf595833532@gmail.com>
2021-11-06 19:40:54 +0800

    add points set

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,5 +1,10 @@
 const period_url = `http://${window.location.host}/period`
+const points_url = `http://${window.location.host}/points`
+// should the same as python period half max len
 const period_half_max_len = 20
+const pts_set = new Set()
+
+const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -33,10 +38,10 @@ function filterOutliers(someArray) {
 function filterPeriod(orig_period){
   if (orig_period.length > Math.floor(period_half_max_len/2)){
     const filter_period = filterOutliers(orig_period)
-    if (filter_period.length != orig_period.length){
-      console.log("orig",orig_period)
-      console.log("filterd", filter_period)
-    }
+    // if (filter_period.length != orig_period.length){
+    //   console.log("orig",orig_period)
+    //   console.log("filterd", filter_period)
+    // }
     return filter_period
   } else {
     return orig_period
@@ -52,54 +57,70 @@ function generateTable(tab, ary) {
     cell_content.appendChild(document.createTextNode(element))
   }
 }
+async function subscribe_period() {
+  const response = await fetch(period_url);
+
+  if (response.status == 502) {
+    // 状态 502 是连接超时错误，
+    // 连接挂起时间过长时可能会发生，
+    // 远程服务器或代理会关闭它
+    // 让我们重新连接
+    await subsccribe_period();
+  } else if (response.status != 200) {
+    console.error(response.statusText);
+    // 一秒后重新连接
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await subscribe_period();
+  } else {
+    const data = await response.json()
+    const period = filterPeriod(data.period)
+    const table = document.getElementById("main-period-tab")
+    table.replaceChildren()
+    if (period.length > 0) {
+      generateTable(table, period)
+      document.getElementById("main-avg").innerHTML = average(period)
+    }
+    // 再次调用 subscribe() 以获取下一条消息
+    // Hold a second
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await subscribe_period();
+  }
+}
+
+
+async function subscribe_pts() {
+  // console.log("Subscribe Points!")
+  const response = await fetch(points_url);
+  // console.log(response);
+
+  if (response.status == 502) {
+    await subsccribe_pts();
+  } else if (response.status != 200) {
+    console.error(response.statusText);
+    // 一秒后重新连接
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await subscribe_pts();
+  } else {
+    const data = await response.json()
+    const pts = data.points
+    // console.log("points", pts)
+    pts.forEach(point => pts_set.add(point))
+    console.log("pts set", pts_set)
+
+    // 再次调用 subscribe() 以获取下一条消息
+    // Hold a second
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await subscribe_pts();
+  }
+}
 
 
 function init() {
   console.log("LOADED!")
 
   // Calculate Average
-  const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
-  async function subscribe_period() {
-    let response = await fetch(period_url);
 
-    if (response.status == 502) {
-      // 状态 502 是连接超时错误，
-      // 连接挂起时间过长时可能会发生，
-      // 远程服务器或代理会关闭它
-      // 让我们重新连接
-      await subsccribe_period();
-    } else if (response.status != 200) {
-      console.error(response.statusText);
-      // 一秒后重新连接
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      await subscribe_period();
-    } else {
-      let data = await response.json()
-      let period = filterPeriod(data.period)
-      const table = document.getElementById("main-period-tab")
-      table.replaceChildren()
-      if (period.length > 0) {
-        generateTable(table, period)
-        document.getElementById("main-avg").innerHTML = average(period)
-      }
-      // 再次调用 subscribe() 以获取下一条消息
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      await subscribe_period();
-    }
-  }
+  subscribe_period();
+  subscribe_pts()
 
-  // fetch(period_url).then(res=> res.json()).then(data => {
-  //   console.log(data)
-  //   document.getElementById("main-period").innerHTML = data.period
-  // })
-
-  
-  subscribe_period()
-  document.getElementById("refresh").addEventListener("click", (e) => {
-    fetch(period_url).then(res => res.json()).then(data => {
-      console.log(data)
-      document.getElementById("main-period").innerHTML = data.period
-      document.getElementById("main-avg").innerHTML = average(data.period)
-    })
-  })
 }
