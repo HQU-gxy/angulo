55c00c6679f8b3505355d9ba15eb1330d3ce95b0
crosstyan <cf595833532@gmail.com>
2021-11-09 10:38:26 +0800

    add service and delete comment

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -4,19 +4,21 @@ const alt_host = "alt-cam.local:5001"
 const alt_period_url = `http://${alt_host}/period`
 const alt_points_url = `http://${alt_host}/extreme_points`
 // should the same as python period half max len
-const PERIOD_HALF_MAX_LEN = 20
+// const PERIOD_HALF_MAX_LEN = 20
 const pts_set = new Set()
 const period_set = new Set()
 const alt_period_set = new Set()
 const alt_pts_set = new Set()
-const main_length_filtered = []
-const alt_length_filtered = []
+
+const main_length_filtered = new Set()
+const alt_length_filtered = new Set()
 
 const MAX_BIN = 64
 
-const PERIOD_FETCH_INTERVAL = 800
+const OUTLINER_MIN = 10
+const PERIOD_FETCH_INTERVAL = 1000
 const POINT_FETCH_INTERVAL = 1000
-const FINAL_LEN_FILTERED_LENGTH = 11
+const FINAL_LEN_FILTERED_LENGTH = 7
 
 let isMainPtsCalc = false
 let isAltPtsCalc = false
@@ -30,7 +32,7 @@ let alt_diff = 0
 // 3cm diff of tube
 const DIFF = 0.075
 
-const STD_MIN = 0.15
+const STD_MIN = 0.2
 let time_ms = 0
 let angle_time_ms = 0
 
@@ -198,9 +200,9 @@ function export_period(){
     openSaveFileDialog(exportedCsvStr,"period.csv","text/csv")
 }
 
-function filterOutliers(someArray) {
+function outlinerFilter(someArray) {
 
-  if(someArray.length < 4)
+  if(someArray.length < OUTLINER_MIN)
     return someArray;
 
   let values, q1, q3, iqr, maxValue, minValue;
@@ -219,20 +221,12 @@ function filterOutliers(someArray) {
   maxValue = q3 + iqr * 1.5;
   minValue = q1 - iqr * 1.5;
 
-  return values.filter((x) => (x >= minValue) && (x <= maxValue));
-}
-
-function filterPeriod(orig_period){
-  if (orig_period.length > Math.floor(PERIOD_HALF_MAX_LEN/2)){
-    const filter_period = filterOutliers(orig_period)
-    // if (filter_period.length != orig_period.length){
-    //   console.log("orig",orig_period)
-    //   console.log("filterd", filter_period)
-    // }
-    return filter_period
-  } else {
-    return orig_period
+  const filtered = values.filter((x) => (x >= minValue) && (x <= maxValue))
+  const difference = someArray.filter(x => !filtered.includes(x))
+  if (difference.length != 0){
+    console.log("outlinerFilter Diff", difference)
   }
+  return filtered
 }
 
 function generateTable(tab, ary) {
@@ -249,44 +243,38 @@ async function subscribe_period() {
     const response = await fetch(period_url);
 
     if (response.status == 502) {
-      // 状态 502 是连接超时错误，
-      // 连接挂起时间过长时可能会发生，
-      // 远程服务器或代理会关闭它
-      // 让我们重新连接
       await subsccribe_period();
     } else if (response.status != 200) {
       console.error(response.statusText);
-      // 一秒后重新连接
       await new Promise(resolve => setTimeout(resolve, 1000));
       await subscribe_period();
     } else {
       const data = await response.json()
-      const period = filterPeriod(data.period)
-      period.forEach(time => period_set.add(time))
-      // Table
-      // const table = document.getElementById("main-period-tab")
-      // table.replaceChildren()
+      const period = data.period
       if (period.length > 0) {
-        // generateTable(table, period)
-        const T_avg = math.mean(period) * 2
-        const length = (T_avg/(2*Math.PI)) ** 2 * 9.8 - DIFF
-        const std = math.std(period)
-        document.getElementById("main-avg").innerHTML = `Period: ${T_avg.toFixed(3)}s STD: ${std}`
-        if(std < STD_MIN && length < 1.6 && length > 0.4){
-          main_length_filtered.push(length)
-          console.log("main", main_length_filtered)
-          if (main_length_filtered.length >= FINAL_LEN_FILTERED_LENGTH){
-            const length_mid = math.median(main_length_filtered)
-            calcLength = length_mid
-            document.getElementById("total-length").innerHTML = `${length_mid.toFixed(3)}m`
-            isPeriodCalc = true
+        const length_array = period.map(time => ((time*2)/(2*Math.PI)) ** 2 * 9.8 - DIFF)
+        const length_array_in_range = length_array.filter(len => (len > 0.4 && len < 1.6))
+        const filtered_length = outlinerFilter(length_array_in_range)
+        if (filtered_length.length > 0) {
+          const length = math.mean(filtered_length)
+          const std = math.std(filtered_length)
+          document.getElementById("main-avg").innerHTML = `STD: ${std}`
+          if(std < STD_MIN && std > 0){
+            main_length_filtered.add(length)
+            console.log("main", main_length_filtered)
+            if (main_length_filtered.size >= FINAL_LEN_FILTERED_LENGTH){
+              const length_ary = Array.from(main_length_filtered)
+              const length_mid = math.median(length_ary)
+              calcLength = length_mid
+              document.getElementById("total-length").innerHTML = `${length_mid.toFixed(3)}m`
+              isPeriodCalc = true
+            }
+            document.getElementById("main-length").innerHTML = `Length: ${length.toFixed(3)}m`
+          } else {
+            document.getElementById("main-length").innerHTML = `Length: Calculating (${length.toFixed(3)}m)`
           }
-          document.getElementById("main-length").innerHTML = `Length: ${length.toFixed(3)}m`
-        } else {
-          document.getElementById("main-length").innerHTML = `Length: Calculating (${length.toFixed(3)}m)`
         }
       }
-      // 再次调用 subscribe() 以获取下一条消息
       // Hold a second
       await new Promise(resolve => setTimeout(resolve, 1000));
       await subscribe_period();
@@ -299,44 +287,38 @@ async function subscribe_alt_period() {
     const response = await fetch(alt_period_url);
 
     if (response.status == 502) {
-      // 状态 502 是连接超时错误，
-      // 连接挂起时间过长时可能会发生，
-      // 远程服务器或代理会关闭它
-      // 让我们重新连接
       await subscribe_alt_period();
     } else if (response.status != 200) {
       console.error(response.statusText);
-      // 一秒后重新连接
       await new Promise(resolve => setTimeout(resolve, 1000));
       await subscribe_alt_period();
     } else {
       const data = await response.json()
-      const period = filterPeriod(data.period)
-      period.forEach(time => period_set.add(time))
-      // Table
-      // const table = document.getElementById("main-period-tab")
-      // table.replaceChildren()
+      const period = data.period
       if (period.length > 0) {
-        // generateTable(table, period)
-        const T_avg = math.mean(period) * 2
-        const length = (T_avg/(2*Math.PI)) ** 2 * 9.8 - DIFF
-        const std = math.std(period)
-        document.getElementById("alt-avg").innerHTML = `Period: ${T_avg.toFixed(3)}s STD: ${std}`
-        if(std < STD_MIN && length < 1.6 && length > 0.4){
-          alt_length_filtered.push(length)
-          console.log("alt", alt_length_filtered)
-          if (alt_length_filtered.length >= FINAL_LEN_FILTERED_LENGTH){
-            const length_mid = math.median(alt_length_filtered)
-            calcLength = length_mid
-            document.getElementById("total-length").innerHTML = `${length_mid.toFixed(3)}m`
-            isPeriodCalc = true
+        const length_array = period.map(time => ((time*2)/(2*Math.PI)) ** 2 * 9.8 - DIFF)
+        const length_array_in_range = length_array.filter(len => (len > 0.4 && len < 1.6))
+        const filtered_length = outlinerFilter(length_array_in_range)
+        if (filtered_length.length > 0) {
+          const length = math.mean(filtered_length)
+          const std = math.std(filtered_length)
+          document.getElementById("alt-avg").innerHTML = `STD: ${std}`
+          if(std < STD_MIN && std > 0){
+            alt_length_filtered.add(length)
+            console.log("alt", alt_length_filtered)
+            if (alt_length_filtered.size >= FINAL_LEN_FILTERED_LENGTH){
+              const length_ary = Array.from(alt_length_filtered)
+              const length_mid = math.median(length_ary)
+              calcLength = length_mid
+              document.getElementById("total-length").innerHTML = `${length_mid.toFixed(3)}m`
+              isPeriodCalc = true
+            }
+            document.getElementById("alt-length").innerHTML = `Length: ${length.toFixed(3)}m`
+          } else {
+            document.getElementById("alt-length").innerHTML = `Length: Calculating (${length.toFixed(3)}m)`
           }
-          document.getElementById("alt-length").innerHTML = `Length: ${length.toFixed(3)}m`
-        } else {
-          document.getElementById("alt-length").innerHTML = `Length: Calculating (${length.toFixed(3)}m)`
         }
       }
-      // 再次调用 subscribe() 以获取下一条消息
       // Hold a second
       await new Promise(resolve => setTimeout(resolve, 1000));
       await subscribe_alt_period();
@@ -398,7 +380,6 @@ async function subscribe_pts() {
         }
       }
 
-      // 再次调用 subscribe() 以获取下一条消息
       // Hold a second
       await new Promise(resolve => setTimeout(resolve, PERIOD_FETCH_INTERVAL));
       await subscribe_pts();
@@ -410,7 +391,6 @@ async function subscribe_alt_pts() {
   if(isAltPtsCalc == false){
     // console.log("Subscribe Points!")
     const response = await fetch(alt_points_url);
-    // console.log(response);
 
     if (response.status == 502) {
       await subscribe_alt_pts();
@@ -431,7 +411,7 @@ async function subscribe_alt_pts() {
           const splited = split_array_in_half(points_arr)
           const left_most_bin = get_most_bin(splited[0])
           const right_most_bin = get_most_bin(splited[1])
-          console.log("alt", left_most_bin, right_most_bin)
+          // console.log("alt", left_most_bin, right_most_bin)
           const left_average = math.mean(left_most_bin)
           const right_average = math.mean(right_most_bin)
           alt_diff = right_average - left_average
